3589aa113e8589b327fa033e807fa3c7079db6bd
TechSammy <mrsam03102002@gmail.com>
2024-01-06 14:50:30 +0530

    Reactjs practice Projects

---
+++ src/accordian/index.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import data  from "./data"
+
+export default function Accordian() {
+  const [selected, seetSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId){
+    console.log(getCurrentId);
+  }
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              {" "}
+              <div  onClick={()=>handleSingleSelection(dataItem.id)} className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div> No data foundd</div>
+        )}
+      </div>
+    </div>
+  );
+}
